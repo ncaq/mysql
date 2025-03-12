2869cdee8b5057291e19bbdd02d419db82e1b9ee
ncaq <ncaq@ncaq.net>
2025-03-12 12:45:19 +0900

    build: add flake files
    The commit adds flake.nix and flake.lock files for nix build configuration with haskell.nix support.

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system overlays;
+          inherit (haskellNix) config;
+        };
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            project = final.haskell-nix.project' {
+              src = ./.;
+              shell = { buildInputs = with pkgs; [ libmysqlclient.dev ]; };
+            };
+          })
+        ];
+        flake = pkgs.project.flake { };
+      in flake);
+
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.nixos.org/"
+      "https://cache.iog.io" # use GHC binary cache.
+    ];
+    extra-trusted-public-keys =
+      [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+    allow-import-from-derivation = "true";
+  };
+}
